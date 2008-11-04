a7782958de2464cc429fbb176c924af8daa4f226
Pierre Bellec <pierre.bellec@gmail.com>
2008-11-04 21:21:09 +0000

    A small demo of pipeline.
    A script to generate important PSOM variables.

---
+++ psom_demo_pipeline.m
@@ -0,0 +1,64 @@
+%
+% _________________________________________________________________________
+% SUMMARY OF NIAK_DEMO_PIPELINE
+% This is a script to demonstrate how to use the pipeline system for Octave
+% and Matlab (PSOM)
+%
+% SYNTAX:
+% Just type in PSOM_DEMO_PIPELINE
+%
+% This is a script and it will clear the workspace !!
+%
+% _________________________________________________________________________
+% COMMENTS:
+%
+% Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008.
+% Maintainer : pbellec@bic.mni.mcgill.ca
+% See licensing information in the code.
+% Keywords : medical imaging, pipeline, fMRI
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+clear
+psom_gb_vars
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Build the pipeline %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+pipeline.message.command = 'fprintf(''The number of samples was : %i. Well that info will be in the logs anyway but still...\n'',opt.nb_samples)';
+pipeline.message.files_in = {};
+pipeline.message.files_out = {};
+pipeline.message.opt.nb_samples = 100;
+
+pipeline.tseries1.command = 'tseries = randn([opt.nb_samples 1]); save(files_out,''tseries'')';
+pipeline.tseries1.files_in = {};
+pipeline.tseries1.files_out = [gb_psom_path_demo 'tseries1.mat'];
+pipeline.tseries1.opt.nb_samples = pipeline.message.opt.nb_samples;
+
+pipeline.tseries2.command = 'tseries = randn([100 1]); save(files_out,''tseries'')';
+pipeline.tseries2.files_in = {};
+pipeline.tseries2.files_out = [gb_psom_path_demo 'tseries2.mat'];
+pipeline.tseries2.opt.nb_samples = pipeline.message.opt.nb_samples;
+
+pipeline.fft.command = 'load(files_in{1}); ftseries = zeros([size(tseries,1) 2]); ftseries(:,1) = fft(tseries); load(files_in{2}); ftseries(:,2) = fft(tseries); save(files_out,''ftseries'')';
+pipeline.fft.files_in = {pipeline.tseries1.files_out,pipeline.tseries2.files_out};
+pipeline.fft.files_out = [gb_psom_path_demo 'ftseries.mat'];
+pipeline.fft.opt = struct([]);
+
---
+++ psom_gb_vars.m
@@ -0,0 +1,94 @@
+
+%% Here are important PSOM variables. Whenever needed, PSOM will call
+%% this script to initialize the variables. If PSOM does not behave the way
+%% you want, this might be the place to fix that.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% The following variables need to be changed to configure the pipeline %%
+%% system                                                               %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+gb_psom_tmp = cat(2,filesep,'tmp',filesep); % where to store temporary files
+
+gb_psom_command_matlab = 'matlab -nojvm -nosplash'; % how to invoke matlab   
+
+gb_psom_command_octave = 'octave-2.9.9'; % how to invoke octave
+
+gb_psom_sge_options = ''; % Options for the sge qsub system, example : '-q all.q@yeatman,all.q@zeus' will force qsub to only use the yeatman workstation;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% The following variables describe the folders and external tools PSOM is using for various tasks %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+gb_psom_viewerps = 'evince'; % program to display ps files
+
+gb_psom_viewerjpg = 'eog'; % program to display jpg files
+
+gb_psom_viewersvg = 'eog'; % program to display svg files
+
+gb_psom_ps2pdf = 'ps2pdf'; % The command to convert ps or eps documents into the pdf file format
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% The following variables should not be changed %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% NIAK version
+gb_psom_version = '0.0'; % 
+
+%% Is the environment Octave or Matlab ?
+if exist('OCTAVE_VERSION')    
+    gb_psom_language = 'octave'; %% this is octave !
+else
+    gb_psom_language = 'matlab'; %% this is not octave, so it must be matlab
+end
+
+%% Get langage version
+if strcmp(gb_psom_language,'octave');
+    gb_psom_language_version = OCTAVE_VERSION;
+else
+    gb_psom_language_version = version;
+end 
+
+%% In which path is PSOM ?
+str_gb_vars = which('psom_gb_vars');
+if isempty(str_gb_vars)
+    error('PSOM is not in the path ! (could not find PSOM_GB_VARS)')
+end
+gb_psom_path_psom = fileparts(str_gb_vars);
+gb_psom_path_psom = [gb_psom_path_psom filesep];
+
+%% In which path is the PSOM demo ?
+gb_psom_path_demo = cat(2,gb_psom_path_psom,'data_demo',filesep);
+
+%% What is the operating system ?
+comp = computer;
+tag_unix = {'SOL2','GLNX86','unix','linux'};
+tag_windaub = {'PCWIN','windows'};
+
+if max(ismember(comp,tag_unix))>0
+    gb_psom_OS = 'unix';
+elseif max(ismember(comp,tag_windaub))>0
+    gb_psom_OS = 'windows';
+else
+    warning('System %s unknown!\n',comp);
+    gb_psom_OS = 'unkown';
+end
+
+%% getting user name.
+switch (gb_psom_OS)
+case 'unix'
+	gb_psom_user = getenv('USER');
+case 'windows'
+	gb_psom_user = getenv('USERNAME');	
+otherwise
+	gb_psom_user = 'unknown';
+end
+
+%% Getting the local computer's name
+switch (gb_psom_OS)
+case 'unix'
+	[gb_psom_tmp_var,gb_psom_localhost] = system('uname -n');
+otherwise
+	gb_psom_localhost = 'unknown';
+end
