c98b0bc09873315df2a64602b457f461bac3aa9d
Pierre Bellec <pierre.bellec@gmail.com>
2008-11-07 23:08:16 +0000

    work in progress ...

--- psom_pipeline_manage.m
+++ psom_pipeline_manage.m
@@ -11,52 +11,57 @@ function [] = psom_pipeline_manage(file_pipeline,opt)
 % _________________________________________________________________________
 % INPUTS:
 %
-% FILE_PIPELINE  
-%       (string) The file name of a .MAT file generated using 
-%       PSOM_PIPELINE_INIT. 
-%              
+% FILE_PIPELINE
+%       (string) The file name of a .MAT file generated using
+%       PSOM_PIPELINE_INIT.
+%
 % OPT
 %       (structure) with the following fields :
 %
-%       ACTION             
-%           (string, default 'run') Available actions :
-%
-%               'run' : start a pipeline. If the pipeline is tagged as 
-%                   already running, the pipeline manager will not do 
-%                   anything.
-%
-%               'restart' : reset all 'running' and 'failed' tags to
-%                   'unfinished' and restart the pipeline from there.
-%
 %       MODE
-%           (string, default 'session') how to execute the pipeline :
+%           (string, default 'session') how to execute the jobs :
 %
-%           'session' 
-%                   the pipeline is executed within the current matlab
-%                   session. Interrupting the pipeline with CTRL-C will 
-%                   result in interrupting the pipeline (you can always do 
-%                   a 'restart' latter). There will be no log file for the
-%                   pipeline itself in this mode.
+%           'session'
+%               the pipeline is executed within the current session. The
+%               current path will be used instead of the one that was
+%               active when initializing the pipeline. There will be no log
+%               files for jobs.
 %
 %           'batch'
-%                   Start the pipeline manager and each job in independent
-%                   matlab sessions. Note that more than one session can be
-%                   started at the same time to take advantage of
-%                   muli-processors machine. Moreover, the pipeline will
-%                   run in the background, you can continue to work, close
-%                   matlab or even unlog from your machine on a linux
-%                   system without interrupting it.
+%               Start the pipeline manager and each job in independent
+%               matlab sessions. Note that more than one session can be
+%               started at the same time to take advantage of
+%               muli-processors machine. Moreover, the pipeline will run in
+%               the background, you can continue to work, close matlab or
+%               even unlog from your machine on a linux system without
+%               interrupting it. The matlab path will be the same as the
+%               one that was active when the pipeline was initialized. Log
+%               files will be created for all jobs.
 %
-%           'sge'
-%                   Use the qsub sge system to process the jobs. The
-%                   pipeline runs in the background.
+%           'qsub'
+%               Use the qsub system (sge or pbs) to process the jobs. The
+%               pipeline runs in the background.
 %
-%           'pbs'   
-%                   Use the qsub pbs system to process the jobs. The
-%                   pipeline runs in the background.
+%       FLAG_BATCH
+%           (boolean, default 1) how to execute the pipeline :
+%
+%           If FLAG_BATCH == 0, the pipeline is executed within the session.
+%               Interrupting the pipeline with CTRL-C will result in
+%               interrupting the pipeline (you can always do a 'restart'
+%               latter). There will be no log file for the pipeline itself
+%               in this mode, but rather a verbose in the command window.
+%
+%           If FLAG_BATCH == 1, the pipeline manager is started as a
+%               separate process in its own Matlab or Octave session using
+%               the 'at' system command. The pipeline will thus run in the
+%               background, you can continue to work, close matlab or
+%               even unlog from your machine on a linux system without
+%               interrupting it. A log file of the pipeline manager
+%               activity is created. You can access it using the action
+%               'monitor' of PSOM_PIPELINE_VISU.
 %
 %       MAX_QUEUED
-%           (integer, default 1 in 'session' and 'batch' modes, Inf in
+%           (integer, default 1 'batch' modes, Inf in 'session',
 %           'sge' and 'pbs' modes)
 %           The maximum number of jobs that can be processed
 %           simultaneously. Some qsub systems actually put restrictions
@@ -72,7 +77,7 @@ function [] = psom_pipeline_manage(file_pipeline,opt)
 %
 % _________________________________________________________________________
 % OUTPUTS:
-% 
+%
 % _________________________________________________________________________
 % SEE ALSO:
 %
@@ -81,6 +86,10 @@ function [] = psom_pipeline_manage(file_pipeline,opt)
 %
 % _________________________________________________________________________
 % COMMENTS:
+%
+% Existing 'running' or 'failed' tags will be removed. Make sure the
+% pipeline is not already running if the background if you do that. That
+% behavior is useful to restart a pipeline that has somehow crashed.
 %
 % Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008.
 % Maintainer : pbellec@bic.mni.mcgill.ca
@@ -114,99 +123,265 @@ end
 
 %% Options
 gb_name_structure = 'opt';
-gb_list_fields = {'action','mode','max_queued','qsub_options'};
-gb_list_defaults = {'run','session',0,''};
+gb_list_fields = {'mode','max_queued','qsub_options'};
+gb_list_defaults = {'session',0,''};
 psom_set_defaults
 
 if max_queued == 0
+
     switch action
-        case {'session','batch'}
+
+        case {'batch'}
             opt.max_queued = 1;
-        case {'sge','pbs'}
+            max_queued = 1;
+        case {'session','sge','pbs'}
             opt.max_queued = Inf;
+            max_queued = Inf;
+
+    end % switch action
+
+end % default of max_queued
+
+%% TAG TODO !!
+%% Add here a block of code to run the pipeline in batch mode ...
+
+try
+
+    %% Hard-coded parameters
+    time_between_checks = 10;
+    nb_checks_per_point = 6;
+
+    %% Generating file names
+
+    [path_logs,name_pipeline,ext_pl] = fileparts(file_pipeline);
+
+    file_pipe_running = cat(2,path_logs,filesep,name_pipeline,'.running');
+    file_pipe_log = cat(2,path_logs,filesep,name_pipeline,'.log');
+    file_pipe_path = cat(2,path_logs,filesep,name_pipeline,'.path_def.mat');
+
+    %% Check for the status of the pipeline
+    if ~exist(file_pipeline,'file') % Does the pipeline exist ?
+        error('Could not find the pipeline file %s. You need to initialize the pipeline using PSOM_PIPELINE_INIT !',file_pipeline);
     end
+
+    if exist(file_pipe_running,'file') % Is the pipeline running ?
+        fprintf('psom:pipeline: A running tag has been found on the pipeline ! This means the pipeline was either running or crashed.\n I will assume it crashed and restart the pipeline.\n')
+        delete(file_running);
+    end
+
+    load(file_pipeline);
+
+    %% Check if all the files necessary to complete the pipeline can be found
+    flag_ready = true;
+
+    for num_j = 1:length(list_jobs)
+
+        name_job = list_jobs{num_j};
+        list_files_needed = files_in.(name_job);
+        list_files_tobe = niak_files2cell(deps.(name_job));
+        list_files_necessary = list_files_needed(~ismember(list_files_needed,list_files_tobe));
+
+        for num_f = 1:length(list_files_necessary)
+            if ~exist(list_files_necessary{num_f})
+                fprintf('The file %s is necessary to run the pipeline, but is unfortunately missing.\n',list_files_necessary{num_f})
+                flag_ready = false
+            end
+        end
+    end
+
+    if ~flag_ready
+        error('Some files are missing, sorry dude I must quit ...')
+    end
+
+    %% Clean up left-over 'running' tags
+    curr_status = psom_job_status(path_logs,list_jobs);
+    mask_running = ismember(curr_status,'running'); % running jobs
+
+    if max(mask_running)>0
+        fprintf('Some running tags were found on some jobs, even though the pipeline itseld had no running tags. That''s not supposed to happen, I reseted all tags to ''unfinished''\n');
+        list_jobs_running = list_jobs(mask_running);
+        for num_f = 1:length(list_jobs_running)
+            file_running = [path_logs filesep list_jobs_running{num_f} '.running'];
+            delete(file_running)
+        end
+    end
+
+    %% Clean up left-over 'failed' tags
+    mask_failed = ismember(curr_status,{'failed'}); % failed jobs
+    if max(mask_failed)>0
+        fprintf('Some failed tags were found on some jobs. I will try to restart them.\n');
+        list_jobs_failed = list_jobs(mask_failed);
+        for num_f = 1:length(list_jobs_failed)
+            file_running = [path_logs filesep list_jobs_running{num_f} '.failed'];
+            delete(file_failed)
+        end
+    end
+
+    %% Clean up log files for unfinished jobs
+    mask_unfinished = ~ismember(curr_status,'finished'); % non finished jobs
+    if max(mask_unfinished)>0
+        list_jobs_unfinished = list_jobs(mask_unfinished);
+        for num_f = 1:length(list_jobs_unfinished)
+            file_log = [path_logs filesep list_jobs_unfinished{num_f} '.log'];
+            if exist(file_log,'file')
+                delete(file_log)
+            end
+        end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%
+    %% Run the pipeline %%
+    %%%%%%%%%%%%%%%%%%%%%%
+
+    fprintf('\nStarting the pipeline ... \n');
+
+    str_now = datestr(clock);
+    save(file_pipe_running,str_now); %% Put a running tag on the pipeline
+    path_tmp = psom_path_tmp(['_',name_pipeline]); % Create a temporary folder for shell scripts
+
+    %% Initialize job status
+    nb_queued = 0;
+    curr_status = psom_job_status(path_logs,list_jobs);
+    mask_running = false(size(list_jobs)); % running jobs (at this stage, none)
+    mask_todo = ~ismember(curr_status,{'finished'}); % done jobs (there is no failed jobs at this stage)
+    mask_todo = mask_todo(:);
+
+    list_jobs_finished = list_jobs(~mask_todo); % Finished jobs
+    for num_f = 1:length(list_jobs_finished) % Remove the dependencies on finished jobs
+        name_job = list_jobs_finished{num_f};
+        for num_f2 = 1:length(list_jobs)
+            name_job2 = list_jobs{num_f2};
+            if isfield(deps.(name_job2),name_job)
+                deps.(name_job2) = rmfield(deps.(name_job2),'name_job');
+            end
+        end
+    end
+
+    mask_deps = false(size(list_jobs)); % jobs that have a dependence on a yet-not-completed job
+    for num_j = 1:length(list_jobs)
+        name_job = list_jobs{num_j};
+        mask_deps(num_j) = ~isempty(deps.(name_job));
+    end
+
+
+    %% Loop until all jobs are done or went bad...
+    while max(mask_todo)>0
+
+        % Update the status of running jobs
+        new_status_running_jobs = psom_job_status(path_logs,list_jobs(mask_running));
+        mask_done(mask_running) = ismember(new_status_running_jobs,{'finished','failed'});
+
+        % Get the list of jobs that failed
+        list_jobs_failed = list_jobs(mask_running);
+        list_jobs_failed = list_jobs_failed(ismember(new_status_running_jobs,'failed'));
+        nb_queued = nb_queued - length(list_jobs_failed);
+
+        % Remove the children of failed jobs from the to-do list
+        for num_f = 1:length(list_jobs_failed)
+            name_job = list_jobs_failed{num_f};
+            list_jobs_child = sub_find_children(list_jobs,name_job,deps);
+            mask_child = ismember(list_jobs,list_jobs_children);
+            mask_todo(mask_child) = false;
+        end
+
+        % Get the list of jobs that were completed
+        list_jobs_finished = list_jobs(mask_running);
+        list_jobs_finished = list_jobs_finished(ismember(new_status_running_jobs,'finished'));
+        nb_queued = nb_queued - length(list_jobs_finished);
+
+        % Remove the dependencies on finished jobs
+        for num_f = 1:length(list_jobs_finished)
+            name_job = list_jobs_finished{num_f};
+            for num_f2 = 1:length(list_jobs)
+                name_job2 = list_jobs{num_f2};
+                if isfield(deps.(name_job2),name_job)
+                    deps.(name_job2) = rmfield(deps.(name_job2),'name_job');
+                end
+            end
+        end
+
+        % Update the dependency mask
+        mask_deps = false(size(list_jobs));
+        for num_j = find(mask_todo)'
+            name_job = list_jobs{num_j};
+            mask_deps(num_j) = ~isempty(deps.(name_job));
+        end
+
+        % Time to submit jobs !!
+
+        while (nb_queued <= max_queued)&max(mask_todo&~mask_deps)>0
+
+            %% Pick up a job to run
+            num_job = find(mask_todo&~mask_deps,1);
+            name_job = list_jobs{num_job};
+            file_job = [path_logs filesep name_job '.mat'];
+            file_log = [path_logs filesep name_job '.log'];
+            mask_todo(num_job) = false;
+            nb_queued = nb_queued + 1;
+
+            %% Create a temporary shell scripts for 'batch' or 'qsub' modes
+            if ~strcmp(mode,'session')
+
+                switch gb_psom_language
+
+                    case 'matlab'
+
+                        instr_job = sprintf(hs,'%s -r ''cd %s, load %s, path(path_work), psom_run_job(%s),''> file_log &\n',gb_psom_command_matlab,path_logs,file_pipe_path,file_job);
+
+                    case 'octave'
+
+                        instr_job = sprintf(hs,'%s --eval ''cd %s, load %s, path(path_work), psom_run_job(%s),''> file_log &\n',gb_psom_command_matlab,path_logs,file_pipe_path,file_job);
+
+                end
+
+            end
+
+            file_shell = [path_tmp filesep name_job '.sh'];
+            hf = fopen(file_shell,'w');
+            fprintf(hf,'%s',instr_job);
+            fclose(hf)
+
+            %% Time to run the job
+            switch mode
+
+                case 'session'
+
+                    psom_run_job(file_job);
+
+                case 'batch'
+
+                    instr_batch = ['at ' file_shell ' -now'];
+                    system(instr_batch);
+
+                case 'qsub'
+
+                    instr_qsub = ['qsub -N ' name_job ' ' opt.opt_qsub ' ' file_shell];
+                    system(instr_qsub);
+
+            end % switch mode
+
+
+        end % submit jobs
+    end
+
+catch
+
+    if exist('path_tmp','var')
+        rmdir(path_tmp,'s'); % Clean the temporary folder
+    end
+
+    if exist('file_pipe_running','var')
+        delete(file_pipe_running); % remove the 'running' tag
+    end
+
+    msg = lasterror;
+    error(msg)
 end
 
-%% Generating file names
+if exist('path_tmp','var')
+    rmdir(path_tmp,'s'); % Clean the temporary folder
+end
 
-[path_logs,name_pipeline,ext_pl] = fileparts(file_pipeline);
-
-file_running = cat(2,path_logs,filesep,name_pipeline,'.running');
-file_log = cat(2,path_logs,filesep,name_pipeline,'.log');
-
-switch action
-    
-    case 'run'
-        
-        if exist(file_running,'file')
-            error('psom:pipeline: A running tag has been found on the pipeline ! This means the pipeline is either running or crashed.\n If it is crashed, try a ''restart'' action instead of ''run''.\n Sorry Dude, I must quit ...')
-        end
-        
-        fprintf('Starting the pipeline ... \n');        
-        
-        hs = fopen(file_run,'w');
-
-        if exist(file_log,'file')
-            system(cat(2,'rm -f ',file_log));
-        end
-        
-        fprintf(hs,'%s run %s %i > %s',file_pipeline,execution_mode,max_queued,file_log);
-        fclose(hs);
-        
-        system(cat(2,'chmod u+x ',file_run));
-        [succ,messg] = system(cat(2,'at -f ',file_run,' now > ',file_start));
-        
-        if succ == 0
-            fprintf('The pipeline was started in the background:\n%s\n',messg);
-        else
-            error(messg)
-        end        
-        
-        
-    case 'restart'
-        
-        fprintf('Cleaning all lock, running and failed jobs ... \n');
-        
-        list_ext = {'running','failed','lock'};
-        
-        for num_e = 1:length(list_ext)
-            
-            list_files = dir(cat(2,path_logs,filesep,'*',list_ext{num_e}));
-            list_files = {list_files.name};
-            
-            for num_f = 1:length(list_files)
-                base_file = list_files{num_f};
-                base_file = base_file(1:end-length(list_ext{num_e}));
-                system(cat(2,'rm -f ',path_logs,filesep,base_file,'log'));
-                system(cat(2,'rm -f ',path_logs,filesep,base_file,list_ext{num_e}));
-            end
-            
-        end                    
-                
-        s = niak_manage_pipeline(file_pipeline,'run',execution_mode)
-        
-    case 'reset'
-        
-        fprintf('Cleaning all lock, running, failed and finished jobs ... \n');        
-        
-        list_ext = {'running','failed','lock','finished'};
-        
-        for num_e = 1:length(list_ext)
-            
-            list_files = dir(cat(2,path_logs,filesep,'*',list_ext{num_e}));
-            list_files = {list_files.name};
-            
-            for num_f = 1:length(list_files)
-                base_file = list_files{num_f};
-                base_file = base_file(1:end-length(list_ext{num_e}));
-                system(cat(2,'rm -f ',path_logs,filesep,base_file,'log'));
-                system(cat(2,'rm -f ',path_logs,filesep,base_file,list_ext{num_e}));
-            end
-            
-        end                    
-               
-        s = niak_manage_pipeline(file_pipeline,'run',execution_mode);        
-        
-    otherwise
-        error('niak:pipeline:%s : unknown action',action);
+if exist('file_pipe_running','var')
+    delete(file_pipe_running); % remove the 'running' tag
 end
