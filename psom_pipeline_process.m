d40bd76cf33c732a1269659e3b4cf7872a96a911
Pierre Bellec <pierre.bellec@gmail.com>
2009-04-13 18:34:17 +0000

    bug fix in the last commit.

--- psom_pipeline_process.m
+++ psom_pipeline_process.m
@@ -166,7 +166,7 @@ end
 %% Options
 gb_name_structure = 'opt';
 gb_list_fields = {'init_matlab','flag_fast','flag_debug','shell_options','command_matlab','mode','mode_pipeline_manager','max_queued','qsub_options','time_between_checks','nb_checks_per_point','time_cool_down'};
-gb_list_defaults = {gb_psom_flag_fast,false,'','','session','',0,'',[],[],[]};
+gb_list_defaults = {gb_psom_init_matlab,gb_psom_flag_fast,false,'','','session','',0,'',[],[],[]};
 psom_set_defaults
 
 if isempty(opt.mode_pipeline_manager)
