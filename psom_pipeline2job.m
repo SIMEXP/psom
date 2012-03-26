1f75eb9f3434b1f0514a67afdb5b2ed96fa9bbfa
Pierre Bellec <pierre.bellec@gmail.com>
2012-03-26 17:29:05 +0000

    Completely redesigned PSOM_PIPELINE2JOB. It is now generating a job structure ready to be plugged in a pipeline structure. It will execute the pipeline using PSOM_PIPELINE_RUN in full session mode.

--- psom_pipeline2job.m
+++ psom_pipeline2job.m
@@ -1,64 +1,54 @@
-function [files_out,files_in,list_jobs] = psom_pipeline2job(pipeline)
-%
-% _________________________________________________________________________
-% SUMMARY PSOM_PIPELINE2JOB
-%
-% Extract the grand total lists of inputs and outputs from a pipeline
-% descrition.
+function [job,cleanup] = psom_pipeline2job(pipeline,path_logs,opt)
+% Convert a pipeline into a job form.
+% This means that a structure ready to be incorporated as a job in a 
+% pipeline will be generated. It will list all input/output files of the 
+% pipeline, and will run the pipeline locally using PSOM.
+% All clean-up jobs are bundled into a separate "job".
 %
 % SYNTAX:
-% [FILES_IN,FILES_OUT,LIST_JOBS] = NIAK_PIPELINE2JOB(PIPELINE)
+% [JOB,CLEANUP] = NIAK_PIPELINE2JOB(PIPELINE,PATH_LOGS,OPT)
 %
 % _________________________________________________________________________
-% INPUTS
+% INPUTS:
 %
 % PIPELINE
-%       (structure) Each field of PIPELINE is a job with an arbitrary name:
+%   (structure) A PSOM pipeline structure.
 %
-%       <JOB_NAME> a structure with the following fields:
+% PATH_LOGS
+%   (string) where to store the logs of the pipeline.
 %
-%               FILES_IN
-%                   (string, cell of strings or structure whos terminal 
-%                   fields are strings or cell of strings)
-%                   a list of the input files of the job
+% OPT
+%   (structure, optional) with the following fields:
 %
-%               FILES_OUT
-%                   (string, cell of strings or structure whos terminal 
-%                   fields are strings or cell of strings)
-%                   a list of the output files of the job
+%   FLAG_CLEANUP
+%       (boolean, default true) if the flag is true, bundle all clean-up 
+%       jobs in a separate job. This is mandatory if multiple 
+%       inter-dependent pipelines are combined together.
 %
+%   FLAG_FILES
+%       (boolean, default true) if the flag is true, add a description of 
+%       input/output files of the pipeline. This is mandatory if multiple 
+%       inter-dependent pipelines are combined together.
+% 
 % _________________________________________________________________________
 % OUTPUTS
 %
-% FILES_OUT
-%       (structure) the field names are identical to PIPELINE
+% JOB
+%    (structure) a single job that recapitulates all input/output files of 
+%    PIPELINE, and will run the full pipeline in a single session.
 %
-%       <JOB_NAME> 
-%           (cell of strings) the list of output files for the job
-%
-% FILES_IN
-%       (structure) the field names are identical to PIPELINE
-%
-%       <JOB_NAME> 
-%           (cell of strings) the list of input files for the job
-%
-%
-% LIST_JOBS
-%       (cell of strings)
-%       The list of all job names
+% CLEANUP 
+%    (structure) all clean-up jobs in PIPELINE are bundled in CLEANUP. The 
+%    clean-up jobs are separated such that combination of multiple pipelines
+%    will run properly. 
 % 
-% _________________________________________________________________________
-% SEE ALSO
-%
-% PSOM_BUILD_DEPENDENCIES
-%
 % _________________________________________________________________________
 % COMMENTS
 %
 % Empty file names, or file names equal to 'gb_niak_omitted' are ignored.
 %
-% Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008.
-% Maintainer : pbellec@bic.mni.mcgill.ca
+% Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008-2012.
+% Maintainer : pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
 % Keywords : pipeline, dependencies
 
@@ -81,31 +71,68 @@ function [files_out,files_in,list_jobs] = psom_pipeline2job(pipeline)
 % THE SOFTWARE.
 
 %% SYNTAX
-if ~exist('pipeline','var')
-    error('SYNTAX: [FILES_IN,FILES_OUT,LIST_JOBS] = PSOM_PIPELINE2JOB(PIPELINE). Type ''help psom_pipeline2job'' for more info.')
+if nargin<2
+    error('SYNTAX: [JOB,CLEANUP] = PSOM_PIPELINE2JOB(PIPELINE,PATH_LOGS,OPT). Type ''help psom_pipeline2job'' for more info.')
 end
 
+list_fields   = { 'flag_cleanup' , 'flag_files' };
+list_defaults = { true           , true         };
+if nargin < 3
+    opt = psom_struct_defaults(struct(),list_fields,list_defaults);
+else
+    opt = psom_struct_defaults(opt,list_fields,list_defaults);
+end
 list_jobs = fieldnames(pipeline);
 nb_jobs = length(list_jobs);
 
-for num_j = 1:nb_jobs
-    name_job = list_jobs{num_j};
-    if nargout > 1
-        if isfield(pipeline.(name_job),'files_in')
-            files_in.(name_job) = psom_files2cell(pipeline.(name_job).files_in);
-        else
-            files_in.(name_job) = {};
+%% Detect and bundle clean-up jobs
+if opt.flag_cleanup
+    cleanup.command = 'psom_clean(files_clean)';
+    cleanup.files_clean = struct();
+    for num_j = 1:nb_jobs
+        if strcmp(pipeline.(list_jobs{num_j}).command,cleanup.command)
+            cleanup.files_clean.(list_jobs{num_j}) = pipeline.(list_jobs{num_j}).files_clean;
         end
     end
-    if isfield(pipeline.(name_job),'files_out')
-        files_out.(name_job) = psom_files2cell(pipeline.(name_job).files_out);
-    else
-        files_out.(name_job) = {};
+    if ~isempty(fieldnames(cleanup.files_clean))
+        pipeline = rmfield(pipeline,fieldnames(cleanup.files_clean));
     end
+    list_jobs = fieldnames(pipeline);
+    nb_jobs = length(list_jobs);
+else
+    cleanup = struct();
 end
 
-files_out = unique(psom_files2cell(files_out));
-if nargout > 2
+if opt.flag_files
+    %% Build files_in
+    for num_j = 1:nb_jobs
+        name_job = list_jobs{num_j};
+        if nargout > 1
+            if isfield(pipeline.(name_job),'files_in')
+                files_in.(name_job) = psom_files2cell(pipeline.(name_job).files_in);
+            else
+                files_in.(name_job) = {};
+            end
+        end
+        if isfield(pipeline.(name_job),'files_out')
+            files_out.(name_job) = psom_files2cell(pipeline.(name_job).files_out);
+        else
+            files_out.(name_job) = {};
+        end
+    end
+
+    %% Build files_out
+    files_out = unique(psom_files2cell(files_out));
     files_in = unique(psom_files2cell(files_in));
     files_in = files_in(~ismember(files_in,files_out));
+    job.files_in = files_in;
+    job.files_out = files_out;
 end
+
+%% Build the job
+job.command = 'psom_run_pipeline(opt.pipeline,opt.psom);';
+job.opt.pipeline = pipeline;
+job.opt.psom.mode = 'session';
+job.opt.psom.mode_pipeline_manager = 'session';
+job.opt.psom.path_logs = path_logs;
+job.opt.psom.flag_pause = false;
