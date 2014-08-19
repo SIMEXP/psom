d44e42f2ca35bc8da71d97bce3adb6f56000f2ca
Pierre Bellec <pierre.bellec@gmail.com>
2014-08-19 12:26:39 -0400

    I have simplified the function that returns job status: it now only cares about .finished, .failed or .running tag files. It is not necessary anymore to specify the execution mode, as the same rules apply regardless of the mode. This is one step towards a solution for issue #1.

--- psom_job_status.m
+++ psom_job_status.m
@@ -1,8 +1,8 @@
-function curr_status = psom_job_status(path_logs,list_jobs,mode_pipe)
+function curr_status = psom_job_status(path_logs,list_jobs)
 % Get the current status of a list of jobs.
 %
 % SYNTAX :
-% CURR_STATUS = PSOM_JOB_STATUS(PATH_LOGS,LIST_JOBS,MODE_PIPE)
+% CURR_STATUS = PSOM_JOB_STATUS(PATH_LOGS,LIST_JOBS)
 %
 % _________________________________________________________________________
 % INPUTS:
@@ -12,11 +12,6 @@ function curr_status = psom_job_status(path_logs,list_jobs,mode_pipe)
 %
 % LIST_JOBS
 %       (cell of strings) a list of job names
-%
-% MODE_PIPE
-%       (string) the execution mode of the pipeline.
-%       Possible values : 'session', 'batch', 'background', 'qsub', 'msub'
-%       'condor'.
 %
 % _________________________________________________________________________
 % OUTPUTS:
@@ -38,27 +33,15 @@ function curr_status = psom_job_status(path_logs,list_jobs,mode_pipe)
 %           'none' : no attempt has been made to process the job yet 
 %                  (neither 'failed', 'running' or 'finished').
 %
-%           'absent' : there is no tag file and no job file. It looks like
-%                   the job name does not exist in the pipeline.
-%
 % _________________________________________________________________________
 % COMMENTS: 
 %
-% The conditions for achieving a 'finished' status vary from one execution
-% mode to another : 
-%   
-%   1. In 'session' mode, the 'finished' status is achieved when a
-%   'finished' tag file can be found in the log folder. This means that the
-%   job has completed without producing any error, and all the outputs have
-%   been created.
-% 
-%   2. In 'batch' mode, in addition of the 'finished' tag file, an 'exit'
-%   tag file is also required, indicating that the batch script has
-%   terminated.
-%
-%  3. In 'qsub' mode, in addition to the 'finished' and 'exit' tag files,
-%  the presence of a 'oqsub' and 'eqsub' log files are required, indicating
-%  that the qsub job was properly terminated.
+% For each job, the presence of tag files is checked (in the following order)
+% to determine the status of the job:
+%  * (job_name).running  : 'running' status
+%  * (job_name).finished : 'finished' status
+%  * (job_name).failed   : 'failed' status
+%  * otherwise           : 'none' status
 %   
 % Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008.
 % Maintainer : pbellec@bic.mni.mcgill.ca
@@ -84,79 +67,34 @@ function curr_status = psom_job_status(path_logs,list_jobs,mode_pipe)
 % THE SOFTWARE.
 
 %% SYNTAX
-if ~exist('path_logs','var') || ~exist('list_jobs','var') || ~exist('mode_pipe','var')
-    error('SYNTAX: CURR_STATUS = PSOM_JOB_STATUS(PATH_LOGS,LIST_JOBS,MODE). Type ''help psom_job_status'' for more info.')
+if ~exist('path_logs','var') || ~exist('list_jobs','var') 
+    error('SYNTAX: CURR_STATUS = PSOM_JOB_STATUS(PATH_LOGS,LIST_JOBS). Type ''help psom_job_status'' for more info.')
 end
 
 %% Loop over all job names, and check for the existence of tag files
 nb_jobs = length(list_jobs);
 curr_status = cell([nb_jobs 1]);
 
-for num_j = 1:nb_jobs        
-    
+for num_j = 1:nb_jobs
+    % Generate the names of relevant tag files
     name_job = list_jobs{num_j};
-        
     file_running  = [path_logs name_job '.running'];
     file_failed   = [path_logs name_job '.failed'];
     file_finished = [path_logs name_job '.finished'];
-    file_exit     = [path_logs name_job '.exit'];
-    file_oqsub    = [path_logs name_job '.oqsub'];
-            
-    flag_exit     = psom_exist(file_exit);
-    flag_oqsub    = psom_exist(file_oqsub);   
+    
+    % Check the presence of tag files
     flag_failed   = psom_exist(file_failed);
     flag_finished = psom_exist(file_finished);            
     flag_running  = psom_exist(file_running); 
     
-    if (flag_finished+flag_failed)>1
-        error('I am confused : job %s has multiple tags. Sorry dude, I must quit ...',name_job);
-    end          
-                     
-   if flag_finished
-        
-        switch mode_pipe
-            case {'qsub','msub','condor'}
-                if flag_oqsub&&flag_exit
-                    curr_status{num_j} = 'finished';
-                else
-                    curr_status{num_j} = 'running';
-                end
-            case 'batch'
-                if flag_exit
-                    curr_status{num_j} = 'finished';
-                else
-                    curr_status{num_j} = 'running';
-                end
-            otherwise
-                curr_status{num_j} = 'finished';
-        end
-
-    elseif flag_failed
-
-        switch mode_pipe
-            case {'qsub','msub','condor'}
-                if flag_oqsub&&flag_exit
-                    curr_status{num_j} = 'failed';
-                else
-                    curr_status{num_j} = 'running';
-                end
-            case 'batch'
-                if flag_exit
-                    curr_status{num_j} = 'failed';
-                else
-                    curr_status{num_j} = 'running';
-                end
-            otherwise
-                curr_status{num_j} = 'failed';
-        end
-
-    elseif flag_running
-
+    % Return status
+    if psom_exist(file_running)
         curr_status{num_j} = 'running';
-
+    elseif psom_exist(file_finished)
+        curr_status{num_j} = 'finished';
+    elseif psom_exist(file_failed)
+        curr_status{num_j} = 'failed';  
     else
-        
-        curr_status{num_j} = 'submitted';        
-                
+        curr_status{num_j} = 'submitted';                
     end
 end
